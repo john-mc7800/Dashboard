23d1e087236bcf84ca5f6e57f90d434a8b3ab6fd
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-10-15 09:05:41 +0500

    colors var

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div >
+      <div className="h-[1009px] w-[276px] bg-orangeColor rounded-xl z-1 relative"></div>
+      <div className="h-[879px] w-[1319px] bg-whiteColor rounded-lg z-2 absolute top-20  "></div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,9 +10,11 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        backgroundColor: "var(--background-color)",
+        orangeColor:"var(--orange-color)",
+        whiteColor:"var(--white-color)",
       },
     },
   },
   plugins: [],
 };
-export default config;
